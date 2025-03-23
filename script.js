d38d7ab2bb4af7a9bd7f811189707031158bec4c
EliiSithh <Stithelijah17@gmail.com>
2025-03-23 19:29:06 -0400

    script.js

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ function replaceText(element) {
   } else if (element.nodeType === Text.TEXT_NODE) {
     if (element.textContent.match(/coronavirus/gi)) {
       const newElement = document.createElement("span");
-      newElement.innerHTML = element.textContent.replace(
-        /(coronavirus)/gi,
-        '<span class="rainbow">$1</span>'
-      );
+      newElement.className = "rainbow";
+      newElement.textContent = element.textContent;
       element.replaceWith(newElement);
     }
   }
 }
 
-// Apply text replacement when the page loads
+// Run the script when the page loads
 document.addEventListener("DOMContentLoaded", () => {
   replaceText(document.body);
 });
-
